b78e7fe472d65c14000e94e5e7c68e68ecf79bb7
gaurav parsar <98256253+INDIANgaurav@users.noreply.github.com>
2025-05-17 16:38:25 +0530

    .

--- server/app.js
+++ server/app.js
@@ -13,6 +13,10 @@ connectDb();
 app.use(cors({ origin: ["task-tracker-sigma-opal.vercel.app"] , credentials: true, }));
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", taskRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
  
 app.listen(PORT, () => {
   console.log("server started : " + PORT);
